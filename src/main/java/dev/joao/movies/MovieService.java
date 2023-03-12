912c9bc0f9f1c5b94ed9b5d770ae981433780671
joaoazevedo25 <jonnyramos10@gmail.com>
2023-03-11 21:45:00 -0300

    Spring Java Crud

---
+++ src/main/java/dev/joao/movies/MovieService.java
@@ -0,0 +1,24 @@
+package dev.joao.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(String imdbId) {
+        return movieRepository.findMovieByImdbId(imdbId);
+    }
+
+    public void deleteMovie(String imdbId) {
+        movieRepository.deleteMovieByImdbId(imdbId);
+    }
+}
---
+++ src/test/java/dev/joao/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joao.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
